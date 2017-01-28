5037b203ba81dca32f5f65b37dc5bd3e29587205
Sivkardo <leonard.pacandi5@gmail.com>
2017-01-28 15:40:14 +0100

    Dodan zadatak iz c++ zadace

---
+++ Koordinatne točke-zadatak.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+
+int main() { 
+	double x1,x2,y1,y2,a,b,c,d,UT;
+	cout<<"Unesene koordinate za tocku A su: ";
+	cin>>x1>>y1;
+	cout<<"Unesene koordinate za tocku B su: ";
+	cin>>x2>>y2;
+	a=(x2-x1);
+	b=(y2-y1);
+	c=pow(a,2);
+	d=pow(b,2);
+	UT=sqrt(c+d);
+	cout<<"Udaljenost tocaka A i B je: "<<UT;
+	cout<<endl;
+		
+	system("PAUSE");
+	return 0;
+}
